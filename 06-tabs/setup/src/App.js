4e427ff846137a4592bbb50d362d551e78d34da6
Abolaji Ayodeji <abolajiayodeji93@gmail.com>
2024-01-11 09:46:31 +0100

    I updated the code with TABs DATA

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <h2>tabs project setup</h2>
+  //adding loading state
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  //loading state
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
+  //desctructure
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>expierence</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* BTN Container */}
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                onClick={() => setValue(index)}
+                key={item.id}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        {/* iterate over our job info because its an array */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {/* iterate over our duties because its an array */}
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
